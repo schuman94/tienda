8a8dc607ac77dcebbbc2da101015951c8ce9c50c
Sergio Chulian <sergio.chulian@iesdonana.org>
2024-11-27 21:12:31 +0100

    27-11-24

---
+++ resources/views/articulos/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Artículos
+        </h2>
+    </x-slot>
+
+Hola
+</x-app-layout>
